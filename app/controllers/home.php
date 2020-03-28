792a43ca8f7b03728b37e21a2be8b3d4ae13902e
aryaman-titan <aryamanbehera@gmail.com>
2020-03-29 03:58:29 +0530

    ajax working successfully and LOGIN/SIGNUP ready

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -6,4 +6,8 @@ class Home{
     public function get(){
         echo \View\Loader::make()->render("templates/home.twig");
     }
+
+    public function get_xhr(){
+        echo "Hello Aryaman! krliya be tu :-)";
+    }
 }
--- r.sh
+++ r.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 composer dump-autoload
 cd public
+google-chrome http://localhost:8000
 php -S localhost:8000
